df32a343607723f69531a7f357ab5ddcf8639566
enesky <eneskamilyilmaz@gmail.com>
2023-10-23 12:15:05 +0300

    Little ui changes

--- app/src/main/java/dev/enesky/doodle/MainActivity.kt
+++ app/src/main/java/dev/enesky/doodle/MainActivity.kt
@@ -3,11 +3,13 @@ package dev.enesky.doodle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.enesky.doodle.ui.theme.DoodleTheme
@@ -17,8 +19,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DoodleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -28,10 +32,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.headlineLarge,
+        )
+    }
 }
 
 @Preview(showBackground = true)
