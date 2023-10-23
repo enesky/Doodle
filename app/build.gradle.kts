2346cc0d1a9d7d026692eabd03980459bc3878f7
enesky <eneskamilyilmaz@gmail.com>
2023-10-24 01:34:05 +0300

    updated: Kotlin vers, Kotlin compiler vers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,15 +34,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+    kotlinOptions.jvmTarget = "17"
+    composeOptions.kotlinCompilerExtensionVersion = "1.5.3"
+    buildFeatures.compose = true
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
