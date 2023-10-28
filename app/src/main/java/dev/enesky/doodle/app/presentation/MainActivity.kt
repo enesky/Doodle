1da7616065e17fab7c9117417685c6494066e54e
enesky <eneskamilyilmaz@gmail.com>
2023-10-29 01:13:11 +0300

    added: MainViewModel usage to MainActivity

--- app/src/main/java/dev/enesky/doodle/app/presentation/MainActivity.kt
+++ app/src/main/java/dev/enesky/doodle/app/presentation/MainActivity.kt
@@ -5,21 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.enesky.doodle.R
 import dev.enesky.doodle.app.presentation.theme.DoodleTheme
+import dev.enesky.doodle.feature.main.MainViewModel
+import org.koin.androidx.compose.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    //private val mainViewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,18 +49,50 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Greeting(
+    modifier: Modifier = Modifier,
+) {
+
+    /*
+    val mainViewModel: MainViewModel?
+    if (!LocalInspectionMode.current) {
+        // We're _not_ executing in an Android Studio Preview.
+        mainViewModel = koinViewModel()
+        mainViewModel.getPopularAnimes()
+    } else {
+        mainViewModel = null
+    }
+    */
+
+
+    val viewModel = getViewModel<MainViewModel>()
+
     Box(
         modifier = modifier.fillMaxWidth(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            modifier = Modifier.basicMarquee(
-                iterations = Int.MAX_VALUE
-            ),
-            text = stringResource(id = R.string.welcome, stringResource(R.string.app_name)),
-            style = MaterialTheme.typography.headlineLarge,
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier.basicMarquee(
+                    iterations = Int.MAX_VALUE
+                ),
+                text = stringResource(id = R.string.welcome, stringResource(R.string.app_name)),
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            Button(
+                modifier = Modifier.padding(32.dp),
+                onClick = {
+                    //mainViewModel?.getPopularAnimes()
+                    viewModel.getAnimeById(1)
+                }
+            ) {
+                Text(text = "Request it", color = Color.White)
+            }
+            
+        }
     }
 }
 
@@ -56,6 +100,11 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 private fun GreetingPreview() {
     DoodleTheme {
-        Greeting()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting()
+        }
     }
 }
