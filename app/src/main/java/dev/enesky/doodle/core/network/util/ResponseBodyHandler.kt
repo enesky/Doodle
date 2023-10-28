8188682eb40dedab5f9c3141c648ccf74135ce12
enesky <eneskamilyilmaz@gmail.com>
2023-10-29 01:06:48 +0300

    added: getBodyOrThrowError() method for getting Response body safely

---
+++ app/src/main/java/dev/enesky/doodle/core/network/util/ResponseBodyHandler.kt
@@ -0,0 +1,17 @@
+package dev.enesky.doodle.core.network.util
+
+import retrofit2.Response
+
+/**
+ * Created by Enes Kamil YILMAZ on 28/10/2023
+ */
+
+inline fun <reified T : Any> Response<T>.getBodyOrThrowError(): T {
+    val errorMessage = "Response body is null."
+
+    return if (isSuccessful) {
+        body() ?: throw IllegalStateException(errorMessage)
+    } else {
+        throw IllegalStateException(errorMessage)
+    }
+}
