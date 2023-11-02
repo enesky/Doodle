907645bf6876ead5d0358c2c9e9265dc1002a878
enesky <eneskamilyilmaz@gmail.com>
2023-11-02 11:24:11 +0300

    updated: DetektConventionPlugin

--- build-logic/convention/src/main/kotlin/DetektConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/DetektConventionPlugin.kt
@@ -1,55 +1,77 @@
 import dev.enesky.build_logic.convention.libs
 import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import java.io.File
 
 /**
  * Detekt Configurations
- *  to use it on normal mode -> ./gradlew detekt
+ *  to use it on normal mode -> ./gradlew detektAll
  *  to use it on steroids -> ./gradlew detektDevPremiumDebug
  *  to use it with auto correct enabled -> ./gradlew detekt / detektDevPremiumDebug --auto-correct
- *  to use it to create a baseline -> ./gradlew detektBaseline / detektBaselineDevPremiumDebug
+ *  to use it to create a baseline -> ./gradlew detekCreateBaseline / detektBaselineDevPremiumDebug
  **/
 
 class DetektConventionPlugin : Plugin<Project> {
 
-    override fun apply(target: Project) = with(target) {
-        val sourcePath: String = rootDir.absolutePath
-        val mainConfigFile = "$rootDir/detekt/config.yml"
-        val composeConfigFile = "$rootDir/detekt/compose-config.yml"
-        val baselineFile = "$rootDir/detekt/baseline.xml"
-        val kotlinFiles = "**/*.kt"
-        val resourceFiles = "**/resources/**"
-        val buildFiles = "**/build/**"
-        val generatedFiles = "**/generated/**"
-        val ideRelatedFiles = "**/.idea/**"
+    override fun apply(project: Project) {
+        project.run {
 
-        pluginManager.apply(
-            libs.findLibrary("detekt-gradle-plugin").get().get().group.toString()
-        )
+            val sourcePath: String = rootDir.absolutePath
+            val mainConfigFile = "$rootDir/detekt/config.yml"
+            val composeConfigFile = "$rootDir/detekt/compose-config.yml"
+            val baselineFile = "$rootDir/detekt/baseline.xml"
+            val kotlinFiles = "**/*.kt"
+            val resourceFiles = "**/resources/**"
+            val buildFiles = "**/build/**"
+            val generatedFiles = "**/generated/**"
+            val ideRelatedFiles = "**/.idea/**"
 
-        dependencies {
-            "detektPlugins"(libs.findBundle("detekt-rules").get())
-        }
+            pluginManager.apply {
+                apply(libs.findLibrary("detekt-gradle-plugin").get().get().group.toString())
+            }
 
-        extensions.configure<DetektExtension> {
-            source.setFrom(fileTree(sourcePath))
-            config.setFrom(files(mainConfigFile, composeConfigFile))
-            baseline = File(baselineFile)
-            ignoreFailures = false
-            parallel = true
-            buildUponDefaultConfig = false
-        }
+            dependencies {
+                "detektPlugins"(libs.findBundle("detekt-rules").get())
+            }
 
-        tasks.withType<Detekt>().configureEach {
-            include(kotlinFiles)
-            exclude(resourceFiles, buildFiles, generatedFiles, ideRelatedFiles)
+            extensions.configure<DetektExtension> {
+
+                tasks.withType<Detekt>().configureEach {
+                    include(kotlinFiles)
+                    exclude(resourceFiles, buildFiles, generatedFiles, ideRelatedFiles)
+                    reports {
+                        html.required.set(false)
+                        xml.required.set(false)
+                        txt.required.set(false)
+                    }
+                }
+
+                tasks.register<Detekt>("detektAll") {
+                    description = "Detects all detekt issues."
+                    setSource(sourcePath)
+                    config.setFrom(files(mainConfigFile, composeConfigFile))
+                    baseline.set(File(baselineFile))
+                    parallel = true
+                }
+
+                tasks.register<DetektCreateBaselineTask>("detektCreateBaseline") {
+                    description = "Creates a new detekt baseline."
+                    parallel.set(true)
+                    ignoreFailures.set(true)
+                    setSource(sourcePath)
+                    config.setFrom(files(mainConfigFile, composeConfigFile))
+                    baseline.set(File(baselineFile))
+                    include(kotlinFiles)
+                    exclude(resourceFiles, buildFiles, generatedFiles, ideRelatedFiles)
+                }
+            }
         }
     }
 
