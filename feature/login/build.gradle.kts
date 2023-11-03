54bd6af1d469a76a089e9f43f63b159e086b194a
enesky <eneskamilyilmaz@gmail.com>
2023-11-03 15:45:14 +0300

    updated: build.gradle.kts of all modules added: base convention plugins to all

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,43 +1,12 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    // Convention Plugins
+    alias(libs.plugins.doodle.android.application)
+    alias(libs.plugins.doodle.android.application.compose)
 }
 
-android {
-    namespace = "dev.enesky.feature.login"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "dev.enesky.feature.login"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
+android.namespace = "dev.enesky.feature.login"
 
 dependencies {
-
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
