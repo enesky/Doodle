cbc7df41f577a4373c489689a8768438a6f56e62
enesky <eneskamilyilmaz@gmail.com>
2023-11-03 15:43:30 +0300

    updated: Convention helpers

--- build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/AndroidCompose.kt
@@ -3,62 +3,24 @@ package dev.enesky.build_logic.convention
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
  * Configure Compose-specific options
  */
-internal fun Project.configureAndroidCompose(
-    commonExtension: CommonExtension<*, *, *, *, *>,
-) {
-    commonExtension.apply {
+internal fun Project.configureAndroidCompose(commonExtension: CommonExtension<*, *, *, *, *>) =
+    with(commonExtension) {
+        defaultConfig.vectorDrawables.useSupportLibrary = true
+
         buildFeatures {
             compose = true
+            buildConfig = true
         }
 
-        composeOptions {
-            kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
-        }
+        composeOptions.kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get().toString()
 
         dependencies {
-            val bom = libs.findLibrary("androidx-compose-bom").get()
+            val bom = libs.compose.bom.get().toString()
             add("implementation", platform(bom))
             add("androidTestImplementation", platform(bom))
-            // Add ComponentActivity to debug manifest
-            add("debugImplementation", libs.findLibrary("androidx.compose.ui.testManifest").get())
         }
     }
-
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            freeCompilerArgs = freeCompilerArgs + buildComposeMetricsParameters()
-        }
-    }
-}
-
-private fun Project.buildComposeMetricsParameters(): List<String> {
-    val metricParameters = mutableListOf<String>()
-    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
-    val relativePath = projectDir.relativeTo(rootDir)
-    val buildDir = layout.buildDirectory.get().asFile
-    val enableMetrics = (enableMetricsProvider.orNull == "true")
-    if (enableMetrics) {
-        val metricsFolder = buildDir.resolve("compose-metrics").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
-        )
-    }
-
-    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
-    val enableReports = (enableReportsProvider.orNull == "true")
-    if (enableReports) {
-        val reportsFolder = buildDir.resolve("compose-reports").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
-        )
-    }
-    return metricParameters.toList()
-}
