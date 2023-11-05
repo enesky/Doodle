ca1f23e7c46fd1cb579bb640f016085db8e21358
enesky <eneskamilyilmaz@gmail.com>
2023-11-05 14:31:09 +0300

    updated: some of the convention plugins

--- build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
@@ -1,5 +1,6 @@
 import com.android.build.gradle.TestExtension
 import dev.enesky.build_logic.convention.configureKotlinAndroid
+import dev.enesky.build_logic.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -9,12 +10,12 @@ class AndroidTestConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.test")
-                apply("org.jetbrains.kotlin.android")
+                apply(libs.plugins.kotlin.android.get().pluginId)
             }
 
             extensions.configure<TestExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 34
+                defaultConfig.targetSdk = libs.versions.target.sdk.get().toString().toInt()
             }
         }
     }
