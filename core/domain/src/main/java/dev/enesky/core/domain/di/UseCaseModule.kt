7885bfe339f091079728c2349b3a183e0bc59f50
enesky <eneskamilyilmaz@gmail.com>
2023-11-06 09:26:18 +0300

    moved: Domain related classes to domain module and network related classes to network module

---
+++ core/domain/src/main/java/dev/enesky/core/domain/di/UseCaseModule.kt
@@ -0,0 +1,26 @@
+package dev.enesky.core.domain.di
+
+import dev.enesky.core.network.repository.JikanRepository
+import dev.enesky.core.domain.usecase.AnimeCharactersUseCase
+import dev.enesky.core.domain.usecase.AnimeUseCase
+import dev.enesky.core.domain.usecase.PopularAnimesUseCase
+import org.koin.dsl.module
+
+/**
+ * Created by Enes Kamil YILMAZ on 28/10/2023
+ */
+
+val useCaseModule = module {
+
+    single<PopularAnimesUseCase> {
+        PopularAnimesUseCase(get<JikanRepository>()::getPopularAnimes)
+    }
+
+    single<AnimeUseCase> {
+        AnimeUseCase(get<JikanRepository>())
+    }
+
+    single<AnimeCharactersUseCase> {
+        AnimeCharactersUseCase { get<JikanRepository>().getCharactersByAnimeId(it) }
+    }
+}
---
+++ core/network/src/main/java/dev/enesky/core/network/repository/JikanDataSourceImpl.kt
@@ -0,0 +1,42 @@
+package dev.enesky.core.network.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import dev.enesky.core.network.api.service.JikanService
+import dev.enesky.core.network.model.Anime
+import dev.enesky.core.network.model.Character
+import dev.enesky.core.network.paging.PopularAnimesPagingSource
+import dev.enesky.core.network.util.getBodyOrThrowError
+import dev.enesky.core.network.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by Enes Kamil YILMAZ on 28/10/2023
+ */
+
+class JikanDataSourceImpl(
+    private val jikanService: JikanService
+) : JikanDataSource {
+
+    override fun getPopularAnimes(): Flow<PagingData<Anime>> {
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            pagingSourceFactory = {
+                PopularAnimesPagingSource(jikanService)
+            }
+        ).flow
+    }
+
+    override suspend fun getAnimeById(animeId: Int): Result<Anime> {
+        return kotlin.runCatching {
+            jikanService.getAnimeById(animeId).getBodyOrThrowError()
+        }
+    }
+
+    override suspend fun getCharactersByAnimeId(animeId: Int): Result<List<Character>> {
+        return kotlin.runCatching {
+            jikanService.getCharactersByAnimeId(animeId).getBodyOrThrowError()
+        }
+    }
+}
