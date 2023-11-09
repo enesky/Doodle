883a6790aaae7e82a585584c86c62934a2700b29
enesky <eneskamilyilmaz@gmail.com>
2023-11-10 00:11:27 +0300

    updated: git hook related files moved to config directory

--- build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/plugins/common/GitHooksPlugin.kt
+++ build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/plugins/common/GitHooksPlugin.kt
@@ -10,7 +10,7 @@ import org.gradle.kotlin.dsl.apply
  */
 class GitHooksPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
-        apply(from = "git-hooks/githooks.gradle")
+        apply(from = "config/git-hooks/githooks.gradle")
 
         /**
          * Uncomment this if you don't use Spotless
