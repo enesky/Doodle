cda491064c0a24f61812659040b3c1beb31ce071
enesky <eneskamilyilmaz@gmail.com>
2023-11-10 00:00:33 +0300

    updated: pre commit-push hooks with detekt+spotless scripts

--- git-hooks/hooks/pre-commit.sh
+++ git-hooks/hooks/pre-commit.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 # same file is added to project/.git/hooks/
 
-echo "*************************************************"
-echo "*         Running Detekt Auto Correct           *"
-echo "*************************************************"
-
-OUTPUT="/tmp/detekt-$(date +%s)"
-./gradlew detektAll --auto-correct > $OUTPUT
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  cat $OUTPUT
-  rm $OUTPUT
-  echo "*************************************************"
-  echo "*         Detekt Auto Correct failed            *"
-  echo "*************************************************"
-  exit $EXIT_CODE
-else
-  echo "*************************************************"
-  echo "*         Detekt Auto Correct failed            *"
-  echo "*************************************************"
-fi
-rm $OUTPUT
+#echo "*************************************************"
+#echo "*         Running Detekt Auto Correct           *"
+#echo "*************************************************"
+#
+#OUTPUT="/tmp/detekt-$(date +%s)"
+#./gradlew detektAll --auto-correct > $OUTPUT
+#EXIT_CODE=$?
+#if [ $EXIT_CODE -ne 0 ]; then
+#  cat $OUTPUT
+#  rm $OUTPUT
+#  echo "*************************************************"
+#  echo "*         Detekt Auto Correct failed            *"
+#  echo "*************************************************"
+#  exit $EXIT_CODE
+#else
+#  echo "*************************************************"
+#  echo "*         Detekt Auto Correct failed            *"
+#  echo "*************************************************"
+#fi
+#rm $OUTPUT
--- git-hooks/hooks/pre-push.sh
+++ git-hooks/hooks/pre-push.sh
@@ -1,24 +1,119 @@
 #!/bin/sh
 # same file is added to project/.git/hooks/
 
-echo "*************************************************"
-echo "*                Running Detekt                 *"
-echo "*************************************************"
+# -------------- Functions --------------
+function run_detekt_auto_correct() {
+  echo "*************************************************"
+  echo "*         Running Detekt Auto Correct           *"
+  echo "*************************************************"
 
-OUTPUT="/tmp/detekt-$(date +%s)"
-./gradlew detektAll > $OUTPUT
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  cat $OUTPUT
+  OUTPUT="/tmp/detekt-$(date +%s)"
+  ./gradlew detektAll --auto-correct > $OUTPUT
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -ne 0 ]; then
+    cat $OUTPUT
+    rm $OUTPUT
+    echo "*************************************************"
+    echo "*         Detekt Auto Correct failed            *"
+    echo "*************************************************"
+    exit $EXIT_CODE
+  else
+    echo "*************************************************"
+    echo "*       Detekt Auto Correct succeeded           *"
+    echo "*************************************************"
+  fi
   rm $OUTPUT
+}
+
+function run_detekt() {
+  echo "-------------------------------------------------"
   echo "*************************************************"
-  echo "*                 Detekt failed                 *"
-  echo "*  Please fix the above issues before pushing   *"
+  echo "*                Running Detekt                 *"
   echo "*************************************************"
-  exit $EXIT_CODE
-else
+
+  OUTPUT="/tmp/detekt-$(date +%s)"
+  ./gradlew detektAll > $OUTPUT
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -ne 0 ]; then
+    cat $OUTPUT
+    rm $OUTPUT
+    echo "*************************************************"
+    echo "*                 Detekt failed                 *"
+    echo "*  Please fix the above issues before pushing   *"
+    echo "*************************************************"
+    exit $EXIT_CODE
+  else
+    echo "*************************************************"
+    echo "*               Detekt succeeded                *"
+    echo "*************************************************"
+  fi
+  rm $OUTPUT
+}
+
+function run_spotless_apply() {
   echo "*************************************************"
-  echo "*               Detekt succeeded                *"
+  echo "*            Running Spotless Apply             *"
   echo "*************************************************"
-fi
-rm $OUTPUT
+
+  OUTPUT="/tmp/detekt-$(date +%s)"
+  ./gradlew spotlessApply > $OUTPUT
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -ne 0 ]; then
+    cat $OUTPUT
+    rm $OUTPUT
+    echo "*************************************************"
+    echo "*            Spotless Apply failed              *"
+    echo "*  Please fix the above issues before pushing   *"
+    echo "*************************************************"
+    exit $EXIT_CODE
+  else
+    echo "*************************************************"
+    echo "*          Spotless Apply succeeded             *"
+    echo "*************************************************"
+  fi
+  rm $OUTPUT
+}
+
+function run_spotless_check() {
+  echo "*************************************************"
+  echo "*            Running Spotless Check             *"
+  echo "*************************************************"
+
+  OUTPUT="/tmp/detekt-$(date +%s)"
+  ./gradlew spotlessCheck > $OUTPUT
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -ne 0 ]; then
+    cat $OUTPUT
+    rm $OUTPUT
+    echo "*************************************************"
+    echo "*            Spotless Check failed              *"
+    echo "*  Please fix the above issues before pushing   *"
+    echo "*************************************************"
+    exit $EXIT_CODE
+    run_spotless_apply
+  else
+    echo "*************************************************"
+    echo "*          Spotless Check succeeded             *"
+    echo "*************************************************"
+  fi
+  rm $OUTPUT
+}
+# --------------  End of Functions --------------
+
+
+# --------------  Running Functions -------------
+echo "-----------------------------------------------------"
+echo "        Spotless & Detekt scripts started...         "
+echo "-----------------------------------------------------"
+run_spotless_check
+echo "-----------------------------------------------------"
+run_detekt_auto_correct
+echo "-----------------------------------------------------"
+run_detekt
+echo "-----------------------------------------------------"
+echo "-        Spotless & Detekt scripts finished.        -"
+echo "-----------------------------------------------------"
