e10089bf3d99f044d47a5921c57dd77d0585678a
enesky <eneskamilyilmaz@gmail.com>
2023-11-09 23:41:32 +0300

    updated: SpotlessPlugin with ktlint rule overrides

--- build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/plugins/common/SpotlessPlugin.kt
+++ build-logic/convention/src/main/kotlin/dev/enesky/build_logic/convention/plugins/common/SpotlessPlugin.kt
@@ -14,40 +14,42 @@ import org.gradle.kotlin.dsl.configure
  *  to check code format and format code -> ./gradlew spotlessCheck spotlessApply
  */
 class SpotlessPlugin : Plugin<Project> {
-    override fun apply(target: Project) = with(target) {
-        pluginManager.apply(libs.plugins.spotless.plugin.get().pluginId)
+  override fun apply(target: Project) = with(target) {
+    pluginManager.apply(libs.plugins.spotless.plugin.get().pluginId)
 
-        extensions.configure<SpotlessExtension> {
-            kotlin {
-                target("**/*.kt")
-                targetExclude("build/**/*.kt", "**/build/**/*.kt", "config/**/*.kt", "${layout.buildDirectory}/**/*.kt")
-                licenseHeaderFile(rootProject.file("config/spotless/licence.kt"))
-                ktlint().editorConfigOverride(
-                    mapOf(
-                        "indent_size" to "2",
-                        "continuation_indent_size" to "2"
-                    )
-                )
-                trimTrailingWhitespace()
-                endWithNewline()
-            }
+    extensions.configure<SpotlessExtension> {
+      kotlin {
+        target("**/*.kt")
+        targetExclude("build/**/*.kt", "**/build/**/*.kt", "config/**/*.kt", "${layout.buildDirectory}/**/*.kt")
+        licenseHeaderFile(rootProject.file("config/spotless/licence.kt"))
+        ktlint().editorConfigOverride(
+          mapOf(
+            "indent_size" to "2",
+            "continuation_indent_size" to "2",
+            "ktlint_standard_function-naming" to "disabled",
+            "ktlint_standard_package-name" to "disabled",
+          ),
+        )
+        trimTrailingWhitespace()
+        endWithNewline()
+      }
 
-            kotlinGradle {
-                target("**/*.gradle.kts")
-                targetExclude("build/**/*.gradle.kts", "**/build/**/*.gradle.kts")
-                licenseHeaderFile(
-                    rootProject.file("config/spotless/licence.kt"),
-                    "(plugins |pluginManagement |import |@file)"
-                )
-            }
+      kotlinGradle {
+        target("**/*.gradle.kts")
+        targetExclude("build/**/*.gradle.kts", "**/build/**/*.gradle.kts")
+        licenseHeaderFile(
+          rootProject.file("config/spotless/licence.kt"),
+          "(plugins |pluginManagement |import |@file)",
+        )
+      }
 
-            format("xml") {
-                target("**/*.xml")
-                targetExclude("build/**/*.xml", "**/build/**/*.xml", "config/**/*.xml")
-                licenseHeaderFile(rootProject.file("config/spotless/licence.xml"), "(<[^!?])")
-            }
+      format("xml") {
+        target("**/*.xml")
+        targetExclude("build/**/*.xml", "**/build/**/*.xml", "config/**/*.xml")
+        licenseHeaderFile(rootProject.file("config/spotless/licence.xml"), "(<[^!?])")
+      }
 
-            lineEndings = LineEnding.PLATFORM_NATIVE
-        }
+      lineEndings = LineEnding.PLATFORM_NATIVE
     }
+  }
 }
