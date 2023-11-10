b0eb24fff8baddcafd1cae24a48db828e46dc0f6
enesky <eneskamilyilmaz@gmail.com>
2023-11-10 09:43:11 +0300

    updated: spotless licence.kt

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -1,5 +1,6 @@
 /*
- * Copyright 2023 Enes Kamil Yılmaz
+ *                          Copyright 2023
+ *            Designed and developed by Enes Kamil Yılmaz
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
