dfad4a0bfb83cacbb8eea5479d71543e4db85ded
enesky <eneskamilyilmaz@gmail.com>
2023-11-17 10:35:55 +0300

    added: light-dark color schemes to Theme

--- core/design-system/src/main/java/dev/enesky/core/design_system/Theme.kt
+++ core/design-system/src/main/java/dev/enesky/core/design_system/Theme.kt
@@ -17,13 +17,17 @@
 package dev.enesky.core.design_system
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -32,7 +36,6 @@ private val LightColors = lightColorScheme(
     surface = LightThemeColors.ebonyClay,
     onSurface = LightThemeColors.white,
     secondary = LightThemeColors.soothingBreeze,
-    onSecondary = LightThemeColors.ebonyClay,
 )
 
 private val DarkColors = darkColorScheme(
@@ -40,24 +43,27 @@ private val DarkColors = darkColorScheme(
     surface = DarkThemeColors.geyser,
     onSurface = DarkThemeColors.black,
     secondary = DarkThemeColors.soothingBreeze,
-    onSecondary = DarkThemeColors.geyser,
 )
 
+
+/**
+ * Dynamic color is available on Android 12+
+ **/
 @Composable
 fun DoodleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        // dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        //    val context = LocalContext.current
-        //    if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        // }
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
         darkTheme -> DarkColors
         else -> LightColors
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
