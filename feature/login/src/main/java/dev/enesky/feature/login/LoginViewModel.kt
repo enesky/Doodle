05aa9b460c1ed2b82fb7ffbe41307c6dc84d77ff
enesky <eneskamilyilmaz@gmail.com>
2023-11-26 01:00:58 +0300

    added: email sign in functions to LoginViewModel

--- feature/login/src/main/java/dev/enesky/feature/login/LoginViewModel.kt
+++ feature/login/src/main/java/dev/enesky/feature/login/LoginViewModel.kt
@@ -10,6 +10,7 @@ import dev.enesky.core.common.delegate.UiState
 import dev.enesky.core.common.delegate.UiStateDelegate
 import dev.enesky.feature.login.manager.AuthManager
 import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
 
 /**
  * Created by Enes Kamil YILMAZ on 11/11/2023
@@ -18,6 +19,51 @@ import kotlinx.coroutines.launch
 class LoginViewModel(
     private val authManager: AuthManager,
 ) : ViewModel(), UiState<LoginUiState> by UiStateDelegate({ LoginUiState() }) {
+
+    // ------------------ EMAIL ------------------
+    fun clickSignInWithEmail(email: String, password: String) {
+        viewModelScope.launch {
+            val signInResult = authManager.signInWithEmailAndPassword(email, password)
+            when {
+                signInResult.data != null -> {
+                    signInEmailResult(signInResult, true)
+                }
+                signInResult.errorMessage?.contains("no user record") == true -> {
+                    signUpWithEmail(email, password)
+                }
+                else -> {
+                    signInEmailResult(signInResult, false)
+                }
+            }
+        }
+    }
+
+    private fun signInEmailResult(
+        signInResult: SignInResult,
+        isSignInSuccessful: Boolean,
+    ) {
+        setState {
+            copy(
+                authType = AuthType.EMAIL,
+                signInResult = signInResult,
+                isSignInSuccessful = isSignInSuccessful,
+            )
+        }
+    }
+
+    private suspend fun signUpWithEmail(email: String, password: String) {
+        val resultFromSignUp: SignInResult = authManager.signUpWithEmailAndPassword(
+            email = email,
+            password = password
+        )
+        if (resultFromSignUp.data != null) {
+            signInEmailResult(resultFromSignUp, true)
+        } else {
+            signInEmailResult(resultFromSignUp, false)
+        }
+    }
+
+    // ------------------ GOOGLE ------------------
 
     fun clickSignInWithGoogle(
         launcher: ManagedActivityResultLauncher<IntentSenderRequest, ActivityResult>,
@@ -31,6 +77,7 @@ class LoginViewModel(
             )
         }
     }
+
     fun signInGoogleWithIntent(intent: Intent) {
         viewModelScope.launch {
             val signInResult = authManager.signInGoogleInitial(intent)
@@ -43,6 +90,8 @@ class LoginViewModel(
             }
         }
     }
+
+    // ------------------ ANONYMOUS ------------------
 
     fun signInAnonymously() {
         viewModelScope.launch {
