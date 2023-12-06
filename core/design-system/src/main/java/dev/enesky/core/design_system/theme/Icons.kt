d638d22001fed81339b6ef754c78cce62bc91c57
enesky <eneskamilyilmaz@gmail.com>
2023-12-06 22:20:17 +0300

    added: Icons object for global icon usages

---
+++ core/design-system/src/main/java/dev/enesky/core/design_system/theme/Icons.kt
@@ -0,0 +1,52 @@
+package dev.enesky.core.design_system.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Bookmarks
+import androidx.compose.material.icons.outlined.Upcoming
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Bookmark
+import androidx.compose.material.icons.rounded.BookmarkBorder
+import androidx.compose.material.icons.rounded.Bookmarks
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Grid3x3
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.rounded.ShortText
+import androidx.compose.material.icons.rounded.Upcoming
+import androidx.compose.material.icons.rounded.ViewDay
+
+/**
+ * Created by Enes Kamil YILMAZ on 06/12/2023
+ */
+
+object Icons {
+
+    val Add = Icons.Rounded.Add
+    val ArrowBack = Icons.Rounded.ArrowBack
+    val Bookmark = Icons.Rounded.Bookmark
+    val BookmarkBorder = Icons.Rounded.BookmarkBorder
+    val Bookmarks = Icons.Rounded.Bookmarks
+    val BookmarksBorder = Icons.Outlined.Bookmarks
+    val Check = Icons.Rounded.Check
+    val Close = Icons.Rounded.Close
+    val Grid3x3 = Icons.Rounded.Grid3x3
+    val MoreVert = Icons.Default.MoreVert
+    val Person = Icons.Rounded.Person
+    val Search = Icons.Rounded.Search
+    val ShortText = Icons.Rounded.ShortText
+    val Upcoming = Icons.Rounded.Upcoming
+    val UpcomingBorder = Icons.Outlined.Upcoming
+    val ViewDay = Icons.Rounded.ViewDay
+
+    // Bottom Navigation Bar Icons
+    val Home = Icons.Rounded.Home
+    val Explore = Icons.Rounded.Search
+    val MyLists = Icons.Rounded.Bookmarks
+    val Settings = Icons.Rounded.Settings
+
+}
