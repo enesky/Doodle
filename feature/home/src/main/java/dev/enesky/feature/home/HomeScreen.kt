cdbe573ea5c5599b020dc45f61e4e31e63567b5a
enesky <eneskamilyilmaz@gmail.com>
2023-12-15 16:55:53 +0300

    added: SwipeResfresh to HomeScreen

--- feature/home/src/main/java/dev/enesky/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/dev/enesky/feature/home/HomeScreen.kt
@@ -6,23 +6,25 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import dev.enesky.core.common.utils.Logger
 import dev.enesky.core.common.utils.ObserveAsEvents
+import dev.enesky.core.design_system.common.SwipeRefresh
 import dev.enesky.core.design_system.theme.DoodleTheme
 import dev.enesky.core.network.model.Anime
 import dev.enesky.core.ui.annotation.PreviewUiMode
 import dev.enesky.core.ui.util.isEmpty
+import dev.enesky.core.ui.util.isLoading
 import dev.enesky.feature.home.helpers.HomeEvents
+import kotlinx.coroutines.flow.emptyFlow
 import org.koin.androidx.compose.koinViewModel
 
 /**
@@ -36,7 +38,7 @@ fun HomeRoute(
     onNavigateDetailsClick: (id: String) -> Unit,
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val popularAnimes = uiState.popularAnimes?.collectAsLazyPagingItems()
+    val popularAnimes = (uiState.popularAnimes ?: emptyFlow()).collectAsLazyPagingItems()
 
     ObserveAsEvents(flow = viewModel.eventFlow) { homeEvents ->
         when (homeEvents) {
@@ -64,51 +66,50 @@ fun HomeRoute(
 @Composable
 private fun HomeScreen(
     modifier: Modifier = Modifier,
-    popularAnimes: LazyPagingItems<Anime>? = null,
+    popularAnimes: LazyPagingItems<Anime>,
     onNavigateDetailsClick: (id: String) -> Unit,
 ) {
-    DoodleTheme {
-        Surface(
-            modifier = modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background,
+    SwipeRefresh(
+        modifier = modifier,
+        isRefreshing = popularAnimes.loadState.refresh.isLoading,
+        onRefresh = popularAnimes::refresh
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(DoodleTheme.spacing.medium),
+            contentPadding = PaddingValues(DoodleTheme.spacing.large),
         ) {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(DoodleTheme.spacing.medium),
-                contentPadding = PaddingValues(DoodleTheme.spacing.large),
-            ) {
-                when {
-                    popularAnimes == null || popularAnimes.isEmpty() -> {
-                        item {
-                            Text(
-                                text = "Loading...",
-                                style = DoodleTheme.typography.regular.h1,
-                            )
-                        }
+            when {
+                popularAnimes.isEmpty() -> {
+                    item {
+                        Text(
+                            text = "Loading...",
+                            style = DoodleTheme.typography.regular.h1,
+                        )
+                    }
+                }
+
+                else -> {
+                    item {
+                        Text(
+                            text = "Popular Animes",
+                            style = DoodleTheme.typography.regular.h1,
+                        )
                     }
 
-                    else -> {
-                        item {
+                    items(popularAnimes.itemCount) { index ->
+                        val anime = popularAnimes[index]
+
+                        if (anime != null) {
+                            AnimeItem(
+                                anime = anime,
+                                onNavigateDetailsClick = onNavigateDetailsClick,
+                            )
+                        } else {
                             Text(
-                                text = "Popular Animes",
+                                text = "Anime item is null :(",
                                 style = DoodleTheme.typography.regular.h1,
                             )
-                        }
-
-                        items(popularAnimes.itemCount) { index ->
-                            val anime = popularAnimes[index]
-
-                            if (anime != null) {
-                                AnimeItem(
-                                    anime = anime,
-                                    onNavigateDetailsClick = onNavigateDetailsClick,
-                                )
-                            } else {
-                                Text(
-                                    text = "Anime item is null :(",
-                                    style = DoodleTheme.typography.regular.h1,
-                                )
-                            }
                         }
                     }
                 }
@@ -131,7 +132,7 @@ private fun AnimeItem(
     ) {
         Text(
             text = anime.title,
-            style = MaterialTheme.typography.headlineLarge,
+            style = DoodleTheme.typography.regular.h1,
         )
     }
 }
@@ -139,5 +140,7 @@ private fun AnimeItem(
 @PreviewUiMode
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen {}
+    HomeScreen(
+        popularAnimes = emptyFlow<PagingData<Anime>>().collectAsLazyPagingItems()
+    ) { }
 }
