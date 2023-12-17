bfe9276c5712e85785d1be7824f0aecc2bc24b16
enesky <eneskamilyilmaz@gmail.com>
2023-12-17 22:26:31 +0300

    removed: unnecessary icons

--- core/design-system/src/main/java/dev/enesky/core/design_system/theme/Icons.kt
+++ core/design-system/src/main/java/dev/enesky/core/design_system/theme/Icons.kt
@@ -11,14 +11,10 @@ import androidx.compose.material.icons.rounded.BookmarkBorder
 import androidx.compose.material.icons.rounded.Bookmarks
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Grid3x3
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material.icons.rounded.ShortText
 import androidx.compose.material.icons.rounded.Upcoming
-import androidx.compose.material.icons.rounded.ViewDay
 
 /**
  * Created by Enes Kamil YILMAZ on 06/12/2023
@@ -34,14 +30,10 @@ object Icons {
     val BookmarksBorder = Icons.Outlined.Bookmarks
     val Check = Icons.Rounded.Check
     val Close = Icons.Rounded.Close
-    val Grid3x3 = Icons.Rounded.Grid3x3
     val MoreVert = Icons.Default.MoreVert
-    val Person = Icons.Rounded.Person
     val Search = Icons.Rounded.Search
-    val ShortText = Icons.Rounded.ShortText
     val Upcoming = Icons.Rounded.Upcoming
     val UpcomingBorder = Icons.Outlined.Upcoming
-    val ViewDay = Icons.Rounded.ViewDay
 
     // Bottom Navigation Bar Icons
     val Home = Icons.Rounded.Home
