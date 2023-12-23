97238c9589ccb75c5fdc75537317d349d76df522
enesky <eneskamilyilmaz@gmail.com>
2023-12-23 23:36:00 +0300

    updated: MiniAnime to Anime

--- core/domain/src/main/java/dev/enesky/core/domain/usecase/TopAnimePagingUseCase.kt
+++ core/domain/src/main/java/dev/enesky/core/domain/usecase/TopAnimePagingUseCase.kt
@@ -1,12 +1,12 @@
 package dev.enesky.core.domain.usecase
 
 import androidx.paging.PagingData
-import dev.enesky.core.data.Anime
-import dev.enesky.core.data.AnimeFilter
+import dev.enesky.core.data.response.AnimeResponse
+import dev.enesky.core.data.models.AnimeFilter
 import kotlinx.coroutines.flow.Flow
 
 /**
  * Created by Enes Kamil YILMAZ on 19/12/2023
  */
 
-fun interface TopAnimePagingUseCase : (AnimeFilter) -> Flow<PagingData<Anime>>
+fun interface TopAnimePagingUseCase : (AnimeFilter) -> Flow<PagingData<AnimeResponse>>
