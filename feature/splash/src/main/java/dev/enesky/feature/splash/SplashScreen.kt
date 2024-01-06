68449234dca4c627e09178fe38985fd2482432fc
enesky <eneskamilyilmaz@gmail.com>
2024-01-06 20:46:49 +0300

    updated: Navigations of SplashScreen

--- feature/splash/src/main/java/dev/enesky/feature/splash/SplashScreen.kt
+++ feature/splash/src/main/java/dev/enesky/feature/splash/SplashScreen.kt
@@ -1,67 +1,100 @@
 package dev.enesky.feature.splash
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.Manifest
+import android.os.Build
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import dev.enesky.core.common.utils.ObserveAsEvents
 import dev.enesky.core.design_system.annotation.PreviewUiMode
 import dev.enesky.core.design_system.components.CenteredBox
 import dev.enesky.core.design_system.theme.DoodleTheme
+import org.koin.androidx.compose.koinViewModel
 
 /**
  * Created by Enes Kamil YILMAZ on 04/01/2024
  */
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun SplashRoute(
     modifier: Modifier = Modifier,
     onNavigateToLoginScreen: () -> Unit,
     onNavigateToHomeScreen: () -> Unit,
+    viewModel: SplashViewModel = koinViewModel(),
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val lifecycleOwner = LocalLifecycleOwner.current
 
+    // TODO: Wait for permission result
+    val notificationPermissionState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            null
+        }
 
+    DisposableEffect(
+        key1 = lifecycleOwner,
+        effect = {
+            val observer = LifecycleEventObserver { _, event ->
+                if (event == Lifecycle.Event.ON_START) {
+                    notificationPermissionState?.launchPermissionRequest()
+                }
+            }
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
+        }
+    )
+
+    ObserveAsEvents(flow = viewModel.eventFlow) {
+        when (it) {
+            is SplashEvents.OnNavigateToLoginScreen -> {
+                onNavigateToLoginScreen()
+            }
+
+            is SplashEvents.OnNavigateToHomeScreen -> {
+                onNavigateToHomeScreen()
+            }
+
+            is SplashEvents.OnError -> TODO()
+        }
+
+    }
 
     SplashScreen(
         modifier = modifier,
+        uiState = uiState,
     )
 }
 
 @Composable
 private fun SplashScreen(
     modifier: Modifier = Modifier,
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = DoodleTheme.colors.background,
-    ) {
-        SplashContent(
-            modifier = Modifier.fillMaxWidth(),
-        )
-    }
-}
-
-@Composable
-private fun SplashContent(
-    modifier: Modifier = Modifier,
+    uiState: SplashUiState = SplashUiState(),
 ) {
     CenteredBox(
         modifier = modifier.fillMaxWidth(),
         contentAlignment = Alignment.Center,
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Text(
-                text = "Welcome to Splash Screen",
-                style = DoodleTheme.typography.regular.h3,
-            )
-        }
+        Icon(
+            painter = painterResource(id = dev.enesky.core.design_system.R.drawable.doodle_icon),
+            tint = DoodleTheme.colors.white,
+            contentDescription = "Doodle Icon",
+        )
     }
 }
 
--- feature/splash/src/main/java/dev/enesky/feature/splash/SplashViewModel.kt
+++ feature/splash/src/main/java/dev/enesky/feature/splash/SplashViewModel.kt
@@ -1,6 +1,7 @@
 package dev.enesky.feature.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.enesky.core.common.delegate.Event
 import dev.enesky.core.common.delegate.EventDelegate
 import dev.enesky.core.common.delegate.IEvent
@@ -8,6 +9,9 @@ import dev.enesky.core.common.delegate.IUiState
 import dev.enesky.core.common.delegate.UiState
 import dev.enesky.core.common.delegate.UiStateDelegate
 import dev.enesky.core.domain.manager.AuthManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Created by Enes Kamil YILMAZ on 05/01/2024
@@ -19,7 +23,14 @@ class SplashViewModel(
     UiState<SplashUiState> by UiStateDelegate(initialState = { SplashUiState() }),
     Event<SplashEvents> by EventDelegate() {
 
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            checkUser()
+        }
+    }
+
     suspend fun checkUser() {
+        delay(1000L)
         triggerEvent {
             if (authManager.isUserLoggedIn()) {
                 SplashEvents.OnNavigateToHomeScreen
