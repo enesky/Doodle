d3dbcccf2c0d2bf201a37e4fcd1792e980bcf39c
enesky <eneskamilyilmaz@gmail.com>
2024-01-08 01:01:56 +0300

    removed: animation on SignInDestination nav

--- feature/login/src/main/java/dev/enesky/feature/login/navigation/LoginDestination.kt
+++ feature/login/src/main/java/dev/enesky/feature/login/navigation/LoginDestination.kt
@@ -2,6 +2,7 @@ package dev.enesky.feature.login.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import dev.enesky.core.navigation.DoodleNavigationDestination
 import dev.enesky.core.navigation.composableWithAnimation
@@ -35,7 +36,7 @@ fun NavGraphBuilder.loginGraph(
         route = LoginDestination.route,
         startDestination = SignInDestination.route,
     ) {
-        composableWithAnimation(
+        composable(
             route = SignInDestination.route,
         ) {
             SignInScreenRoute(
