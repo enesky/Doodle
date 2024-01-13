9c269e42dfaa54db28748f38915742df592f1309
enesky <eneskamilyilmaz@gmail.com>
2024-01-13 22:11:03 +0300

    removed: unnecessary code

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -14,14 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
 dependencyResolutionManagement {
     repositories {
         google()
