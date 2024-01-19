b4cadcfa4e14993cd4ccf2d3f9137e718791c3ec
enesky <eneskamilyilmaz@gmail.com>
2024-01-20 01:55:02 +0300

    added: build type badge to top right corner

--- app/src/main/java/dev/enesky/doodle/app/ui/DoodleApp.kt
+++ app/src/main/java/dev/enesky/doodle/app/ui/DoodleApp.kt
@@ -9,6 +9,8 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -20,12 +22,14 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.enesky.core.design_system.components.TransparentSystemBars
 import dev.enesky.core.design_system.theme.DoodleTheme
+import dev.enesky.doodle.BuildConfig
 import dev.enesky.doodle.app.navigation.DoodleNavHost
 import dev.enesky.doodle.app.ui.component.DoodleBottomBar
 import dev.enesky.doodle.app.ui.component.DoodleSnackbarHost
@@ -74,9 +78,12 @@ fun DoodleApp(
             ) { innerPadding ->
                 TransparentSystemBars()
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = DoodleTheme.colors.background,
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            color = DoodleTheme.colors.background,
+                        ),
                 ) {
                     /**
                      * TODO: Loading screen
@@ -95,6 +102,19 @@ fun DoodleApp(
                         onBackClick = appState::onBackClick,
                         onShowMessage = { message -> appState.showMessage(message) },
                     )
+
+                    // Add build type indicator on top right corner
+                    if (BuildConfig.BUILD_TYPE != "release") {
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .padding(top = DoodleTheme.spacing.xLarge)
+                                .background(color = DoodleTheme.colors.red),
+                            text = "  ${BuildConfig.BUILD_TYPE}    ",
+                            color = DoodleTheme.colors.white,
+                            style = DoodleTheme.typography.regular.h7,
+                        )
+                    }
                 }
             }
         }
